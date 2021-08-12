24db691666039b812e19962c0569d6ea1cadcd74
Nate Nelson <nathan.nelson99@gmail.com>
2021-08-12 14:34:59 -0600

    third commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5858
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../aboutMe.html')))
+app.use(express.static('client'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/home.html')))
 
 app.listen(port, () => console.log(`Server on ${port} is running away!`))
